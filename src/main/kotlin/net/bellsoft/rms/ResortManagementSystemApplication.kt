ab05d4f5da39661ed17f53b1e568c2291a62190d
Bell <bell04204@gmail.com>
2023-10-20 02:52:32 +0900

    feat: 스프링 진입 지점 커버리지 측정 제외

--- src/main/kotlin/net/bellsoft/rms/ResortManagementSystemApplication.kt
+++ src/main/kotlin/net/bellsoft/rms/ResortManagementSystemApplication.kt
@@ -1,11 +1,13 @@
 package net.bellsoft.rms
 
+import net.bellsoft.bellsafehouse.annotation.ExcludeFromJacocoGeneratedReport
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class ResortManagementSystemApplication
 
+@ExcludeFromJacocoGeneratedReport
 fun main(args: Array<String>) {
     runApplication<ResortManagementSystemApplication>(*args)
 }
---
+++ src/main/kotlin/net/bellsoft/rms/annotation/ExcludeFromJacocoGeneratedReport.kt
@@ -0,0 +1,6 @@
+package net.bellsoft.bellsafehouse.annotation
+
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
+annotation class ExcludeFromJacocoGeneratedReport
